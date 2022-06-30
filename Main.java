1bb52b4bea436b7aa6907591751289fb03cf2a41
Aitor C. Figueroa <aitorcf@gmail.com>
2022-06-30 10:51:26 +0200

    Ejercicios tema 4

---
+++ Main.java
@@ -0,0 +1,59 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Ejercicio if
+        int numerolf = 0;
+
+        if (numerolf < 0) {
+            System.out.println("El número es negativo");
+        } else if (numerolf > 0) {
+            System.out.println("El número es positivo");
+        } else {
+            System.out.println("El número es cero");
+        };
+
+        //Ejercicio While
+        int numeroWhile = 0;
+
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        };
+
+        //Ejercicio Do While
+        int numeroDoWhile = 2;
+
+        do {
+            System.out.println(numeroDoWhile)
+            numeroDoWhile++
+        } while (numeroDoWhile < 3);
+
+        //Ejercicio For
+        for(numeroFor = 0; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor);
+        };
+
+        //Ejercicio Switch
+        var estacion = "verano";
+
+        switch(estacion) {
+            case "invierno":
+                System.out.println("Es invierno");
+                break;
+            case "primavera":
+                System.out.println("Es primavera");
+                break;
+            case "verano":
+                System.out.println("Es verano");
+                break;
+            case "otoño":
+                System.out.println("Es otoño");
+                break;
+            case default:
+                System.out.println(estacion);
+                break;
+        }
+    }
+
+}
